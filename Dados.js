9235391be5aed8a8b57af3573f5a282eda261b47
YasmimSilva0202 <152650731+YasmimSilva0202@users.noreply.github.com>
2024-04-02 15:22:28 -0300

    Dados Adicionados
    Adicionei dados fictícios no arquivo Dados.JS

---
+++ Dados.js
@@ -0,0 +1,7 @@
+var users = [
+    { name:"Lucas", age: 6, height: 120},
+    {name: "Carol", age: 98, height: 159},
+    {name: "André", age: 15, height: 174},
+    {name: "Givanio", age: 35, height: 175},
+
+]
